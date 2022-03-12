a2f5abe299799b80be0f7813ba5c1528e7331a4f
Andrew Lounsbury <lounsburya@gmail.com>
2022-03-12 14:54:07 -0600

    worked out calculate function probably

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,13 +2,51 @@ console.log('js working');
 
 $(handleReady);
 
+let operator = '';
+
 function handleReady() {
-    console.log('jq working');
-    
-  $('equal').on('click', handleEqual)  
+  console.log('jq working');
+
+  $('#equal').on('click', handleEqual)
+getCalcInputs();
 }
 
-function handleEqual(){
-let
+function handleEqual() {
+  console.log('click works');
 
+  let inputOne = $('#inputOne').val();  //takes first input
+  let inputTwo = $('#inputTwo').val();  //takes second input
+  $.ajax({
+    url: '/math',
+    method: 'POST',
+    data: {
+      theInputOne: inputOne,  // ajax posts this data (from inputs) on server
+      selector: selector,
+      theInputTwo: inputTwo,
+    }
+  }).then(function (response) {
+    console.log(response);
+  })
+
+}
+function getCalcInputs(){
+  console.log('Getting Calculator Inputs');
+  $.ajax({
+    url: '/math',
+    method: 'GET'
+
+
+}).then(function (response) {
+    console.log(response);
+    render(response);
+  })
+}
+
+function render(inputs) {
+
+  $('#inputs').empty();
+
+  for (let input of inputs) {
+    $('#inputs').append(`<li>${input.theInputOne} ${input.theInputTwo}</li>`)
+  }
 }
--- server/server.js
+++ server/server.js
@@ -3,12 +3,61 @@ const bodyParser = require('body-parser');
 const app = express();
 const PORT = 5000;
 
+let inputVals = [];
+let outputVals = []
 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static('server/public'));
 
 app.listen(PORT, () => {
-    console.log ('Server is running on port', PORT)
-  })
-  
+  console.log('Server is running on port', PORT)
+})
+
+app.post('/math', (req, res) => {
+  console.log('POST inputs', req.body);
+
+  inputVals.push(req.body);
+
+  console.log(inputVals);
+  res.sendStatus(201);
+
+  calculate(req.body)
+})
+
+app.get('/math', (req, res) => {
+  console.log('Get inputVals');
+  res.send(inputVals)
+})
+
+function calculate(solution) {
+  let inputOne = (solution.theInputOne);
+  let selector = (solution.selector);
+  let inputTwo = (solution.theInputTwo);
+ 
+  switch (selector) {
+  case '+':
+    answer = inputOne + inputTwo;
+    inputVals.push(`${inputOne} + ${inputTwo} = ${answer}`)
+    outputVals.push(answer);
+    break;
+
+  case '-':
+    answer = inputOne - inputTwo;
+    inputVals.push(`${inputOne} - ${inputTwo} = ${answer}`)
+    outputVals.push(answer);
+    break;
+
+    case '/':
+    answer = inputOne / inputTwo;
+    inputVals.push(`${inputOne} / ${inputTwo} = ${answer}`)
+    outputVals.push(answer);
+    break;
+
+      case '*':
+    answer = inputOne * inputTwo;
+    inputVals.push(`${inputOne} * ${inputTwo} = ${answer}`)
+    outputVals.push(answer);
+    break;
+  }
+}
