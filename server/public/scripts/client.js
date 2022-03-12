d8ef746af0e20b2a5849b3bd66d10c1f55603ed9
Andrew Lounsbury <lounsburya@gmail.com>
2022-03-11 18:56:06 -0600

    created files/folders, layed out html,  checked if js and jquery were working

---
+++ server/public/scripts/client.js
@@ -0,0 +1,14 @@
+console.log('js working');
+
+$(handleReady);
+
+function handleReady() {
+    console.log('jq working');
+    
+  $('equal').on('click', handleEqual)  
+}
+
+function handleEqual(){
+let
+
+}
